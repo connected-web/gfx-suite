ae47ae66448d66f629d32252f20ca0c85b35b08a
John Beech <johnbeech.github@mkv25.net>
2024-07-25 21:31:21 +0100

    Add status page

--- website/src/pages.ts
+++ website/src/pages.ts
@@ -3,18 +3,20 @@ import UserDetails from './pages/UserDetails.vue'
 import RequestQueue from './pages/RequestQueue.vue'
 import CreateRequest from './pages/CreateRequest.vue'
 import BrowseImages from './pages/BrowseImages.vue'
+import Status from './pages/Status.vue'
 
 const primaryNav = [
   { path: '/', component: Home, icon: 'home', primaryNav: true, navTitle: 'Home' },
   { path: '/user/details', component: UserDetails, icon: 'user-shield' },
   { path: '/create', component: CreateRequest, icon: 'paint-roller', primaryNav: true, navTitle: 'Create' },
   { path: '/browse', component: BrowseImages, icon: 'image', primaryNav: true, navTitle: 'Browse' },
-  { path: '/queue', component: RequestQueue, icon: 'rectangle-list', primaryNav: true, navTitle: 'Requests' }
+  { path: '/status', component: Status, icon: 'chart-simple', primaryNav: true, navTitle: 'Status' }
 ]
 
 export default [
   ...primaryNav,
   { path: '/user', component: Home },
   { path: '/browse/:dateCode', props: true, component: BrowseImages },
-  { path: '/browse/:dateCode/:requestId', props: true, component: BrowseImages }
+  { path: '/browse/:dateCode/:requestId', props: true, component: BrowseImages },
+  { path: '/queue', component: RequestQueue, icon: 'rectangle-list', navTitle: 'Requests' }
 ]
