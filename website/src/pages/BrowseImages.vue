1eb2e5b327a772f6cfa2cebcb974613b4b82f71f
John Beech <johnbeech.github@mkv25.net>
2024-08-01 21:10:34 +0100

    Refactor to create RequestDetails component

--- website/src/pages/BrowseImages.vue
+++ website/src/pages/BrowseImages.vue
@@ -25,59 +25,7 @@
         <span>{{ resultsError?.message }}</span>
       </div>
       <div v-else class="column p10">
-        <h3 class="row">
-          <Icon icon="circle-plus" />
-          <label>Positive prompt</label>
-        </h3>
-        <pre><code>{{ resultsItem?.originalRequest?.positive }}</code></pre>
-        <h3 class="row">
-          <Icon icon="circle-minus" />
-          <label>Negative prompt</label>
-        </h3>
-        <pre><code>{{ resultsItem?.originalRequest?.negative }}</code></pre>
-
-        <h3 class="row">
-          <Icon icon="cog" />
-          <label>Request Settings</label>
-        </h3>
-        <div class="column p5">
-          <div class="row p5 key-value">
-            <label>Width</label>
-            <label>{{ resultsItem?.originalRequest?.width }}</label>
-          </div>
-          <div class="row p5 key-value">
-            <label>Height</label>
-            <label>{{ resultsItem?.originalRequest?.height }}</label>
-          </div>
-          <div class="row p5 key-value">
-            <label>Batch size</label>
-            <label>{{ resultsItem?.originalRequest?.batchSize }}</label>
-          </div>
-        </div>
-          
-        <h3 class="row">
-          <Icon icon="timeline" />
-          <label>Timeline</label>
-        </h3>
-        <div class="column p5">
-          <div class="row p5 key-value">
-            <label>Request Created</label>
-            <label>{{ resultsItem?.originalRequest?.requestTime ?? 'Waiting on server...' }}</label>
-          </div>
-          <div class="row p5 key-value">
-            <label>Processing Started</label>
-            <label>{{ resultsItem?.started ?? 'n/a' }}</label>
-          </div>
-          <div class="row p5 key-value">
-            <label>Processing Finished</label>
-            <label>{{ resultsItem?.finished ?? 'n/a' }}</label>
-          </div>
-          <div class="row p5 key-value">
-            <label>Results Uploaded</label>
-            <label>{{ resultsItem?.uploaded ?? 'n/a' }}</label>
-          </div>
-        </div>
-        <pre v-if="false"><code>{{ resultsItem }}</code></pre>
+        <RequestDetails :resultsItem="resultsItem" />
         
         <h3 class="row">
           <Icon icon="image" />
@@ -135,6 +83,7 @@ import { ImageUtils } from '../clients/ImageUtils'
 
 import LoadingSpinner from '../components/LoadingSpinner.vue'
 import RequestBrowser from './components/RequestBrowser.vue'
+import RequestDetails from './components/RequestDetails.vue'
 
 const imagesApiClient = new ImagesApiClient()
 
@@ -142,7 +91,7 @@ let imageUtils: ImageUtils
 let reloadTimeout: number
 
 export default {
-  components: { LoadingSpinner, RequestBrowser },
+  components: { LoadingSpinner, RequestBrowser, RequestDetails },
   props: {
     dateCode: {
       type: String,
@@ -289,25 +238,6 @@ export default {
 </script>
 
 <style scoped>
-.key-value > *:first-child {
-  flex: 1;
-}
-.key-value > * {
-  min-width: 20vw;
-  font-family: monospace;
-  padding: 4px;
-  background: #eee;
-}
-
-.key-value > label:nth-child(2) {
-  font-weight: bold;
-}
-
-pre {
-  margin: 0;
-  text-wrap: wrap;
-}
-
 .image-placeholder {
   background: #f0f0f0;
 }
---
+++ website/src/pages/components/RequestDetails.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="request-details column p10">
+    <h3 class="row">
+      <Icon icon="circle-plus" />
+      <label>Positive prompt</label>
+    </h3>
+    <pre><code>{{ resultsItem?.originalRequest?.positive }}</code></pre>
+    <h3 class="row">
+      <Icon icon="circle-minus" />
+      <label>Negative prompt</label>
+    </h3>
+    <pre><code>{{ resultsItem?.originalRequest?.negative }}</code></pre>
+
+    <h3 class="row">
+      <Icon icon="cog" />
+      <label>Request Settings</label>
+    </h3>
+    <div class="column p5">
+      <div class="row p5 key-value">
+        <label>Width</label>
+        <label>{{ resultsItem?.originalRequest?.width }}</label>
+      </div>
+      <div class="row p5 key-value">
+        <label>Height</label>
+        <label>{{ resultsItem?.originalRequest?.height }}</label>
+      </div>
+      <div class="row p5 key-value">
+        <label>Batch size</label>
+        <label>{{ resultsItem?.originalRequest?.batchSize }}</label>
+      </div>
+    </div>
+      
+    <h3 class="row">
+      <Icon icon="timeline" />
+      <label>Timeline</label>
+    </h3>
+    <div class="column p5">
+      <div class="row p5 key-value">
+        <label>Request Created</label>
+        <label>{{ resultsItem?.originalRequest?.requestTime ?? 'Waiting on server...' }}</label>
+      </div>
+      <div class="row p5 key-value">
+        <label>Processing Started</label>
+        <label>{{ resultsItem?.started ?? 'n/a' }}</label>
+      </div>
+      <div class="row p5 key-value">
+        <label>Processing Finished</label>
+        <label>{{ resultsItem?.finished ?? 'n/a' }}</label>
+      </div>
+      <div class="row p5 key-value">
+        <label>Results Uploaded</label>
+        <label>{{ resultsItem?.uploaded ?? 'n/a' }}</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ImageResults } from '../../clients/ImagesApi'
+
+export default {
+  props: {
+    resultsItem: {
+      type: Object,
+      default() {
+        return {} as Partial<ImageResults>
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.key-value > *:first-child {
+  flex: 1;
+}
+.key-value > * {
+  min-width: 20vw;
+  font-family: monospace;
+  padding: 4px;
+  background: #eee;
+}
+
+.key-value > label:nth-child(2) {
+  font-weight: bold;
+}
+
+pre {
+  margin: 0;
+  text-wrap: wrap;
+}
+</style>
